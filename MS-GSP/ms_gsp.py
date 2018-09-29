05aaf65a8dd2692aad68452ce3292d1f89411c54
abdulmuqeethmohammed <amoham62@uic.edu>
2018-09-29 17:21:08 -0500

    moving files

---
+++ MS-GSP/ms_gsp.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+def ms_gsp(s, min_supp):
+	m = sort()
+	l = init_pass(m, s)
+
+def sort():
+	pass
+
+def init_pass(m, s):
+	pass
+
+def lvl2_candidate_gen():
+	pass
+
+def ms_candidate_gen():
+	pass
+
+def main():
+	min_supp = []
+	s = []
+	ms_gsp(s, min_supp)
+
+if __name__ == "__main__":
+	main()
