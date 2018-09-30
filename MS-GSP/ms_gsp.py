e7a6c9adcf6072b7f32b00f1316f00edb7736bf1
abdulmuqeethmohammed <amoham62@uic.edu>
2018-09-30 18:42:35 -0500

    implementing sort according to min supports

--- MS-GSP/ms_gsp.py
+++ MS-GSP/ms_gsp.py
@@ -4,7 +4,7 @@ def parse_data(filename):
 	f = open(filename, 'r')
 	lines = f.readlines()
 	f.close
-	
+	I =[]
 	#print(lines)
 	sequences = []
 	for line in lines:
@@ -30,11 +30,13 @@ def parse_data(filename):
 				for i in b:
 					if(i!=''):
 						itemset.append(i.strip())
+						if(i.strip() not in I):
+							I.append(i.strip())
 				sequence.append(itemset)
 		sequences.append(sequence)
-	
+	print(I)
 	print(sequences)
-	return sequences
+	return sequences , I
 
 
 def parse_supports(filename):
@@ -52,23 +54,53 @@ def parse_supports(filename):
 		element2 = line[1].strip()
 
 		if(element1 == 'SDC'):
-			threshold = element2
+			sdc = element2
 		else:
 			element1 = element1.split('(')
 			element1 = element1[1].split(')')
 			item = element1[0].strip()
-			dictionary[item] = element2
+			dictionary[item] = float(element2)
 
 	print(dictionary)
-	print (threshold)
-	return dictionary, threshold
+	print (sdc)
+	return dictionary, sdc
+
+def pack_sequence(sequence):
+	#[[10,40],[50]]
+	seq = '<'
+	for element in sequence:
+		#[10,40]
+		itemset = '{'
+		for el in element:
+			#10
+			itemset = itemset + str(el) + ' '
+		itemset = itemset + '}'
+		seq = seq + itemset
+	seq = seq + '>'
+	print(seq)
+	return seq
 
 def ms_gsp(s, min_supp):
 	m = sort()
 	l = init_pass(m, s)
 
-def sort():
-	pass
+def sort(I,MIS):
+	
+	for i in I :
+		if(i not in MIS):
+			MIS[str(i)] = 0.0
+
+	s = sorted(MIS.items(), key=lambda x: x[1])
+	
+	sorted_items = []
+	sorted_minsup = []
+	
+	for i in s:
+		sorted_items.append(i[0]) 
+		sorted_minsup.append(i[1])
+	#print(sorted_items)
+	#print(sorted_minsup)
+	return sorted_items, sorted_minsup
 
 def init_pass(m, s):
 	pass
@@ -83,8 +115,10 @@ def main():
 	min_supp = []
 	s = []
 	#ms_gsp(s, min_supp)
-	#parse_data('data.txt')
-	parse_supports('supports.txt')
+	sequences, I = parse_data('data.txt')
+	mis, sdc = parse_supports('supports.txt')
+	#pack_sequence([[10,40],[50]])
+	sort(I, mis)
 
 if __name__ == "__main__":
 	main()
