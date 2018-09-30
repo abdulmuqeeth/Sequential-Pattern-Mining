5177e4889cb15047ed2debe0b469c4063dea0a59
sndpkiran <sndpkiran@gmail.com>
2018-09-29 19:25:17 -0500

    wrote lvl2_candidate gen() method

--- MS-GSP/ms_gsp.py
+++ MS-GSP/ms_gsp.py
@@ -63,9 +63,10 @@ def parse_supports(filename):
 	print (threshold)
 	return dictionary, threshold
 
-def ms_gsp(s, min_supp):
+def ms_gsp(s, min_supp, n):
 	m = sort()
 	l = init_pass(m, s)
+	lvl2_candidate_gen(l, min_supp, n, val)
 
 def sort():
 	pass
@@ -73,8 +74,15 @@ def sort():
 def init_pass(m, s):
 	pass
 
-def lvl2_candidate_gen():
-	pass
+def lvl2_candidate_gen(f, min_supp, n, val):
+	candidate_list = []
+
+	for i in range(len(f):
+		if supp(f[i]) >= min_supp[f[i]] # array index could be different
+			for j in range(i + 1, len(f)):
+				if supp(f[j]) >= min_supp[str(f[j])] and ((supp(f[j]) - supp(f[i])) <= val):
+					candidate_list.append([f[i], f[j]])
+	return candidate_list
 
 def ms_candidate_gen():
 	pass
@@ -82,9 +90,10 @@ def ms_candidate_gen():
 def main():
 	min_supp = []
 	s = []
-	#ms_gsp(s, min_supp)
-	#parse_data('data.txt')
+	parse_data('data.txt')
 	parse_supports('supports.txt')
+	n = 0
+	ms_gsp(s, min_supp, n)
 
 if __name__ == "__main__":
 	main()
